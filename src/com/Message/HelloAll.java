0f6b73748a68bc00bcb7745b64ec58d08073fcdd
payalghusalikar <72148385+payalghusalikar@users.noreply.github.com>
2020-11-13 09:43:11 +0530

    Update HelloAll.java

--- src/com/Message/HelloAll.java
+++ src/com/Message/HelloAll.java
@@ -4,7 +4,7 @@ public class HelloAll {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello All");
+		System.out.println("Hello All to class");
 	}
 
 }
